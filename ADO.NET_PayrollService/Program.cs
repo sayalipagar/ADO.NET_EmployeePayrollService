d47606f1c280b24eee66550841fbcb54ebc9cd88
sayalipagar <pagarsayali565@gmail.com>
2022-11-03 14:42:48 +0530

    [Sayali]ADD:Welcome TO ADO.NET PayrollService

---
+++ ADO.NET_PayrollService/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADO.NET_PayrollService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To ADO.NET!");
+        }
+    }
+}
